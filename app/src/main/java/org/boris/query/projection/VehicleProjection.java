c6017098577e038c122be44de682d9f6b1a2ed4c
Bartłomiej Rodak <bartlomiej.rodak97@gmail.com>
2023-07-14 10:08:22 +0200

    telemetics updating on view

--- app/src/main/java/org/boris/query/projection/VehicleProjection.java
+++ app/src/main/java/org/boris/query/projection/VehicleProjection.java
@@ -2,6 +2,7 @@ package org.boris.query.projection;
 
 import org.axonframework.eventhandling.EventHandler;
 import org.boris.core_api.NewVehicleAddedEvent;
+import org.boris.core_api.VehicleTelematicsUpdatedEvent;
 import org.boris.query.entity.Vehicles;
 import org.boris.query.repository.VehiclesRepository;
 import org.slf4j.Logger;
@@ -28,5 +29,25 @@ public class VehicleProjection {
 
         vehiclesRepository.save(view);
         LOGGER.info("Vehicles [{}] has been saved", vehicleReg);
+    }
+
+    @EventHandler
+    public void on(VehicleTelematicsUpdatedEvent event) {
+        final String vehicleId = event.getVehicleReg().getIdentifier();
+
+        var vehicleOpt = vehiclesRepository.findByVehicleReg(vehicleId);
+        var view = vehicleOpt.orElseGet(() -> {
+            LOGGER.warn("Missing vehicle [{}] record, creating new one", vehicleId);
+            return createVehiclesView(vehicleId, event.getTelematicsEnabled());
+        });
+        vehiclesRepository.save(view);
+        LOGGER.info("Telematics for vehicle [{}] switched to {}", vehicleId, event.getTelematicsEnabled());
+    }
+
+    private Vehicles createVehiclesView(String vehicleReg, Boolean telematics) {
+        var view = new Vehicles();
+        view.setVehicleReg(vehicleReg);
+        view.setTelematics(telematics);
+        return view;
     }
 }
